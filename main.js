a8f5668569ef6f3ff65403b98927d04ef7a0e3d4
Erin Woody <erinelizabethwoody@gmail.com>
2017-07-28 13:42:53 -0500

    question2

--- main.js
+++ main.js
@@ -20,9 +20,30 @@ function question1() {
 
 
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
-function question2 () {
-  // Answer:
-}
+function question2() {
+  var cost = [];
+  for (i = 0; i < data.length; i++) 
+    if (data[i].price >= 14 && data[i].price <= 18) {
+      cost.push(data[i].title)
+    }
+     
+  console.log(cost.join("\n"));
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
